795e10d99982363629815e8a0702312ccb72fe4c
Bence Lovas <bansoul1@gmail.com>
2017-09-24 15:31:55 +0200

    almost dry

--- static/scripts/game.js
+++ static/scripts/game.js
@@ -13,27 +13,34 @@ Array.prototype.double = function() {
 }
 
 const config = {
+    flipBackOfCard: "perspective(600px) rotateY(-180deg)",
+    flipBackOfCardBack: "perspective(600px) rotateY(0deg)",
+    flipFrontOfCard: "perspective(600px) rotateY(0deg)",
+    flipFrontOfCardBack: "perspective(600px) rotateY(180deg)",
+    backOfCard: "back",
+    frontOfCard: "front",
     coverColor: "black",
-    flippedSelector: "[data-flipped=true]",
+    flippedCardSelector: "[data-flipped=true]",
     htmlTag: "div",
     attributeToSet: "class",
     removed: "removed",
     eventToListen: "click",
     victoryString: "Congratulations!",
+    alertString: "Please select from options.",
     commonClass: "card",
     colorsArray: ["#679327","#3A1B0F","#A4DA65","#5A3810","#990515","#250D3B","#E0082D","#50105A"],
 };
 
 var game = {
-    flippedCards: 0,
+    matchedCards: 0,
     firstClick: true,
-    cards: document.getElementsByClassName(config.commonClass),
     firstCardColor: null,
     secondCardColor: null,
     columnNumber: null,
     rowNumber: null,
 };
 
+game.getCards = function() { return document.getElementsByClassName(config.commonClass); };
 game.getTotalCardsNumber = function() { return game.columnNumber * game.rowNumber; };
 game.getColorsNeeded = function() { return game.getTotalCardsNumber() / 2; };
 
@@ -48,10 +55,10 @@ game.getRandomizedArray = function() {
 
 game.flipCard = function(clickedCard) {
     for (let i = 0; i < clickedCard.childNodes.length; i++) {
-        if (clickedCard.childNodes[i].className === "back") {
-            clickedCard.childNodes[i].style.transform = "perspective(600px) rotateY(-180deg)";
-        } else if (clickedCard.childNodes[i].className === "front") {
-            clickedCard.childNodes[i].style.transform = "perspective(600px) rotateY(0deg)";
+        if (clickedCard.childNodes[i].className === config.backOfCard) {
+            clickedCard.childNodes[i].style.transform = config.flipBackOfCard;
+        } else if (clickedCard.childNodes[i].className === config.frontOfCard) {
+            clickedCard.childNodes[i].style.transform = config.flipFrontOfCard;
         }
     }
     clickedCard.dataset.flipped = true;
@@ -69,33 +76,32 @@ game.clickProcess = function(clickedCard) {
     }
 };
 
-game.selectFlippedCards = function() { return document.querySelectorAll(config.flippedSelector); };
-game.flipCardsBack = function () {
-    const cards = game.selectFlippedCards();
-    for (let i = 0; i < cards.length; i++) {
-        for (let j = 0; j < cards[i].childNodes.length; j++) {
-            if (cards[i].childNodes[j].className === "back") {
-                cards[i].childNodes[j].style.transform = "perspective(600px) rotateY(0deg)";
-            } else if (cards[i].childNodes[j].className === "front") {
-                cards[i].childNodes[j].style.transform = "perspective(600px) rotateY(180deg)";
+game.selectFlippedCards = function() { return document.querySelectorAll(config.flippedCardSelector); };
+game.flipCardsBack = function (cardsArray) {
+    for (let i = 0; i < cardsArray.length; i++) {
+        for (let j = 0; j < cardsArray[i].childNodes.length; j++) {
+            if (cardsArray[i].childNodes[j].className === config.backOfCard) {
+                cardsArray[i].childNodes[j].style.transform = config.flipBackOfCardBack;
+            } else if (cardsArray[i].childNodes[j].className === config.frontOfCard) {
+                cardsArray[i].childNodes[j].style.transform = config.flipFrontOfCardBack;
             }
         }
-        cards[i].dataset.flipped = false;
+        cardsArray[i].dataset.flipped = false;
     }
 };
 
 game.isCardMatch = function() { return game.firstCardColor === game.secondCardColor; };
-game.isGameOver = function() { return game.getTotalCardsNumber() === game.flippedCards; };
+game.isGameOver = function() { return game.getTotalCardsNumber() === game.matchedCards; };
 
-game.addEventListenerToArray = function(array, callbackFunction) {
+game.addEventListenerToArray = function(array) {
     for (let i = 0; i < array.length; i++) {
-        array[i].addEventListener(config.eventToListen, callbackFunction);
+        array[i].addEventListener(config.eventToListen, game.turnProcess);
     }
 };
 
-game.removeEventListenerFromArray = function(array, callbackFunction) {
+game.removeEventListenerFromArray = function(array) {
     for (let i = 0; i < array.length; i++) {
-        array[i].removeEventListener(config.eventToListen, callbackFunction);
+        array[i].removeEventListener(config.eventToListen, game.turnProcess);
     }
 };
 
@@ -107,10 +113,9 @@ game.removeCards = function(array) {
     }
 };
 
-game.processMatch = function(callbackFunction) {
-    const cards = game.selectFlippedCards();
-    game.removeCards(cards);
-    game.addEventListenerToArray(game.cards, callbackFunction);
+game.processMatch = function() {
+    game.removeCards(game.selectFlippedCards());
+    game.addEventListenerToArray(game.getCards(), game.turnProcess);
 }
 
 game.turnProcess = function() {
@@ -119,25 +124,25 @@ game.turnProcess = function() {
         this.removeEventListener(config.eventToListen, game.turnProcess);
     } else {
         game.clickProcess(this);
-        game.removeEventListenerFromArray(game.cards, game.turnProcess);
+        game.removeEventListenerFromArray(game.getCards());
         if (game.isCardMatch()) {
-            game.flippedCards += 2;
+            game.matchedCards += 2;
             setTimeout(function() {
-                game.processMatch(game.turnProcess);
+                game.processMatch();
             }, 500);
             if (game.isGameOver()) {
                 setTimeout(function() {
                     alert(config.victoryString);
-                    game.flipCardsBackAfterWin();
+                    game.flipCardsBack(document.getElementsByClassName(config.removed));
                 }, 500);
                 setTimeout(function() {
-                    game.clearBoardAfterWin();
+                    game.clearBoard();
                 }, 1000);
             }
         } else {
             setTimeout(function() {
-                game.flipCardsBack();
-                game.addEventListenerToArray(game.cards, game.turnProcess);
+                game.flipCardsBack(game.selectFlippedCards());
+                game.addEventListenerToArray(game.getCards());
             }, 1000);
         }
     }
@@ -147,13 +152,12 @@ game.createCard = function(color) {
     let card = document.createElement(config.htmlTag);
     let cardBack = document.createElement(config.htmlTag);
     let cardFront = document.createElement(config.htmlTag);
-    cardBack.classList.add("back");
-    cardFront.classList.add("front");
+    cardBack.classList.add(config.backOfCard);
+    cardFront.classList.add(config.frontOfCard);
     cardBack.style.backgroundColor = config.coverColor;
     cardFront.style.backgroundColor = color;
     card.dataset.color = color;
     card.dataset.flipped = false;
-    // card.classList.add(config.commonClass, "col", "col-xs-4", "col-sm-2", "col-md-2", "mr-auto");
     card.classList.add(config.commonClass);
     card.appendChild(cardFront);
     card.appendChild(cardBack);
@@ -192,60 +196,43 @@ game.initializeGame = function() {
 }
 
 game.resetData = function() {
-    game.flippedCards = 0;
+    game.matchedCards = 0;
     game.firstClick = true;
-    game.cards = document.getElementsByClassName(config.commonClass);
     game.firstCardColor = null;
     game.secondCardColor = null;
     game.columnNumber = null;
     game.rowNumber = null;
 }
 
-game.flipCardsBackAfterWin = function() {
-    const cards = document.getElementsByClassName(config.removed);
-    for (let i = 0; i < cards.length; i++) {
-        for (let j = 0; j < cards[i].childNodes.length; j++) {
-            if (cards[i].childNodes[j].className === "back") {
-                cards[i].childNodes[j].style.transform = "perspective(600px) rotateY(0deg)";
-            } else if (cards[i].childNodes[j].className === "front") {
-                cards[i].childNodes[j].style.transform = "perspective(600px) rotateY(180deg)";
-            }
-        }
-    }
-}
-
-game.clearBoardAfterWin = function() {
-    const board = document.getElementById("gameDiv");
-    if (board !== null) {
-        const cards = document.getElementsByClassName(config.removed);
-        for (let i = 0; i < cards.length; i++) {
-            for (let j = 0; j < cards[i].childNodes.length; j++) {
-                cards[i].childNodes[j].style.opacity = "0";
-            }
-        }
-        setTimeout(function() {
-            board.parentNode.removeChild(board);
-        }, 500);
-    }
-}
-
-
 game.clearBoard = function() {
     const board = document.getElementById("gameDiv");
     if (board !== null) {
-        board.parentNode.removeChild(board);
+        if (game.isGameOver()) {
+            console.log("im here");
+            const cards = document.getElementsByClassName(config.removed);
+            for (let i = 0; i < cards.length; i++) {
+                for (let j = 0; j < cards[i].childNodes.length; j++) {
+                    cards[i].childNodes[j].style.opacity = "0";
+                }
+            }
+            setTimeout(function() {
+                board.parentNode.removeChild(board);
+            }, 500);
+        } else {
+            board.parentNode.removeChild(board);
+        }
     }
 }
 
 game.processForm = function() {
-    game.resetData();
     game.clearBoard();
+    game.resetData();
     const columnNumber = document.getElementById("columnNumber");
     game.columnNumber = parseInt(columnNumber.options[columnNumber.selectedIndex].value);
     const rowNumber = document.getElementById("rowNumber");
     game.rowNumber = parseInt(rowNumber.options[rowNumber.selectedIndex].value);
     if (isNaN(game.rowNumber) || isNaN(game.columnNumber)) {
-        alert("Please select from options.");
+        alert(config.alertString);
     } else {
         game.initializeGame();
     }
@@ -253,7 +240,7 @@ game.processForm = function() {
 
 game.play = function() {
     const playButton = document.getElementById("play");
-    playButton.addEventListener("click", function(event) {
+    playButton.addEventListener(config.eventToListen, function(event) {
         event.preventDefault();
         game.processForm();
     });
